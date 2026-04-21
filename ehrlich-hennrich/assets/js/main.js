0d311c929af4831aaaab8dbb129b2fac9064bbc4
pytcal <142577883+pytcal@users.noreply.github.com>
2026-04-21 15:25:00 +0200

    Add files via upload

---
+++ ehrlich-hennrich/assets/js/main.js
@@ -0,0 +1,9 @@
+import { initNav } from "./nav.js";
+import { initReveal } from "./reveal.js";
+import { initForm } from "./form.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initNav();
+  initReveal();
+  initForm();
+});
