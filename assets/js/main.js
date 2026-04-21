8d29210d81e01fd22a7a3fab18209e504205cbbe
pytcal <142577883+pytcal@users.noreply.github.com>
2026-04-21 16:28:44 +0200

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initNav } from "./nav.js";
-import { initReveal } from "./reveal.js";
-import { initForm } from "./form.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initNav();
-  initReveal();
-  initForm();
-});
